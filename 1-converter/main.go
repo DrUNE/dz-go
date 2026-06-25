1d8d53f6bb4dc3be1f0c462a7c9fa62e81d3454a
Andrey Litvinov <drune.email@gmail.com>
2026-06-25 15:17:03 +0300

    добавлен ввод валюты

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -11,16 +11,65 @@ const (
 )
 
 func main() {
-	fmt.Println(eur_to_rub)
-
+	amount, currencyFrom, currencyTo := readUserInput()
+	convertedAmount := convertCurrency(amount, currencyFrom, currencyTo)
+	fmt.Printf("Результат конвертации: %.2f %s = %.2f %s", amount, currencyFrom, convertedAmount, currencyTo)
 }
 
-func readUserInput() (amount float64) {
-	fmt.Scan(&amount)
+func readUserInput() (amount float64, currencyFrom string, currencyTo string) {
+
+	for {
+		fmt.Printf("Введите валюту (usd/eur/rub): ")
+		fmt.Scan(&currencyFrom)
+		if currencyFrom == "usd" || currencyFrom == "eur" || currencyFrom == "rub" {
+			break
+		}
+		fmt.Printf("Введеная валюта (%s) неверна, повторите ввод.\n", currencyFrom)
+	}
+
+	for {
+		fmt.Printf("Введите число: ")
+		_, err := fmt.Scan(&amount)
+		if err != nil {
+			fmt.Println(err)
+			fmt.Printf("scaned: %f,  Неверное число, повторите ввод\n", amount)
+		} else {
+			if amount > 0 {
+				break
+			} else {
+				fmt.Printf("Введите число больше 0\n")
+			}
+		}
+	}
+
+	for {
+		fmt.Printf("Введите валюту в которую произойдет конвертация (usd/eur/rub): ")
+		fmt.Scan(&currencyTo)
+		if currencyTo == "usd" || currencyTo == "eur" || currencyTo == "rub" {
+			break
+		}
+		fmt.Printf("Введеная валюта (%s) неверна, повторите ввод.\n", currencyTo)
+	}
+
 	return
 }
 
-func convertCurrency(amount float64, currencyFrom string, currencyTo string) float64 {
-	fmt.Printf("selected currencyes %s %s", currencyFrom, currencyTo)
-	return amount
+func convertCurrency(amount float64, currencyFrom string, currencyTo string) (convertedAmount float64) {
+	switch {
+	case currencyFrom == "usd" && currencyTo == "eur":
+		convertedAmount = amount * usd_to_eur
+	case currencyTo == "usd" && currencyFrom == "eur":
+		convertedAmount = amount / usd_to_eur
+	case currencyFrom == "usd" && currencyTo == "rub":
+		convertedAmount = amount * usd_to_rub
+	case currencyTo == "usd" && currencyFrom == "rub":
+		convertedAmount = amount / usd_to_rub
+	case currencyFrom == "eur" && currencyTo == "rub":
+		convertedAmount = amount * eur_to_rub
+	case currencyTo == "eur" && currencyFrom == "rub":
+		convertedAmount = amount / eur_to_rub
+	default:
+		convertedAmount = amount
+	}
+	return
 }
