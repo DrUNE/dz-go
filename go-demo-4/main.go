406a0e29be290655e11560c1013e0859af2d3b5e
Andrey Litvinov <drune.email@gmail.com>
2026-07-13 14:08:40 +0300

    структуры

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -1,15 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"net/url"
+)
 
-func main() {
-	a := [4]int{1, 2, 3, 4}
-	reverse(&a)
-	fmt.Println(a)
+var runes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$%*&#")
+
+type Account struct {
+	Login    string
+	Password string
+	Url      string
 }
 
-func reverse(a *[4]int) {
-	for i, j := 0, len(*a)-1; i < j; i, j = i+1, j-1 {
-		(*a)[i], (*a)[j] = (*a)[j], (*a)[i]
+func (a *Account) generatePassword(length int) {
+	a.Password = generatePassword(length)
+}
+func newAccount(login, urlString, password string) (*Account, error) {
+	if login == "" {
+		return nil, errors.New("INVALID_LOGIN")
 	}
+
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+
+	account := &Account{
+		Login:    login,
+		Url:      urlString,
+		Password: password,
+	}
+	if account.Password == "" {
+		account.generatePassword(12)
+	}
+	return account, nil
+}
+func main() {
+	login := promptData("Введите логин")
+	urlString := promptData("Введите URL")
+	password := promptData("Введите пароль")
+	myAccount, err := newAccount(login, urlString, password)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(myAccount)
+}
+
+func promptData(s string) string {
+	var input string
+	fmt.Print(s + ": ")
+	fmt.Scanln(&input)
+	return input
+}
+
+func generatePassword(length int) string {
+	password := make([]rune, length)
+	for i := range length {
+		password[i] = runes[rand.Intn(len(runes))]
+	}
+	return string(password)
 }
